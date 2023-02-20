a0df09ba59ffd6cf209ad1b279b1b856f51c83d4
DaehyunKim <74559561+Darren4641@users.noreply.github.com>
2023-02-20 15:11:07 +0900

    [Silver V] Title: K번째 수, Time: 2096 ms, Memory: 380088 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        try {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int[] num = new int[Integer.parseInt(st.nextToken())];
+            int find = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine(), " ");
+            for(int i=0; i < num.length; i++) {
+                num[i] = Integer.parseInt(st.nextToken());
+            }
+            Arrays.sort(num);
+            bw.write(num[find - 1] + "");
+            bw.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
