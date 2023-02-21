4b79d421f89521c1808b5b5f77b767e69035b4be
DaehyunKim <74559561+Darren4641@users.noreply.github.com>
2023-02-21 23:10:06 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 652 ms, Memory: 144544 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,60 +1,64 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    static ArrayList<Integer>[] A;
-    static boolean visited[];
+    static List<Integer>[] A;
+    static boolean[] visited;
+    public static int solution(int[] num, int size) {
+        int answer = 0;
+        init(num, size);
 
-    public static int solution() {
-        int count = 0;
-
-        for(int i = 1; i <A.length; i++) {
-            if(!visited[i]) {
-                count++;
-                DFS(i);
-            }
+        for(int i = 1; i <= size; i++) {
+            if(!visited[i])
+                answer += DFS(A[i]);
         }
-        return count;
+
+        return answer;
     }
 
-    public static void DFS(int v) {
-        if(visited[v]) return;
+    public static int DFS(List<Integer> a) {
+        for(int value : a) {
+            if(!visited[value]) {
+                visited[value] = true;
+                DFS(A[value]);
+            }
+        }
+        return 1;
+    }
 
-        visited[v] = true;
-
-        for(int i : A[v]) {
-            if(!visited[i])
-                DFS(i);
+    public static void init(int[] num, int size) {
+        A = new ArrayList[size + 1];
+        visited = new boolean[size + 1];
+        for(int i = 1; i <= size; i++) {
+            A[i] = new ArrayList<>();
+        }
+        for(int i = 0; i < num.length; i = i + 2) {
+            A[num[i]].add(num[i + 1]);
+            A[num[i + 1]].add(num[i]);
         }
     }
 
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int u,v;
 
         try {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             int size = Integer.parseInt(st.nextToken());
-            int edgeSize = Integer.parseInt(st.nextToken());
-            visited = new boolean[size + 1];
-            A = new ArrayList[size + 1];
-            //인접리스트 생성
-            for(int i = 1; i <= size; i++) {
-                A[i] = new ArrayList<Integer>();
-            }
+            int edge = Integer.parseInt(st.nextToken());
 
-            //인접리스트 연결
-            for(int i = 0; i < edgeSize; i++) {
+
+            int[] num = new int[edge * 2];
+
+            for(int i = 0; i < num.length; i = i + 2) {
                 st = new StringTokenizer(br.readLine(), " ");
-                u = Integer.parseInt(st.nextToken());
-                v = Integer.parseInt(st.nextToken());
-                A[u].add(v);
-                A[v].add(u);
+                num[i] = Integer.parseInt(st.nextToken());
+                num[i + 1] = Integer.parseInt(st.nextToken());
             }
 
-            bw.write(solution() + "");
+            bw.write(String.valueOf(solution(num, size)));
             bw.close();
         } catch(IOException e) {
             e.printStackTrace();
