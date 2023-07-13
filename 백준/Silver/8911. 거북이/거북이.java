1847c6d585baf0bcef3f773f0c165c8435553996
DaehyunKim <74559561+Darren4641@users.noreply.github.com>
2023-07-13 11:23:12 +0900

    [Silver III] Title: 거북이, Time: 428 ms, Memory: 22636 KB -BaekjoonHub

--- 백준/Silver/8911. 거북이/거북이.java
+++ 백준/Silver/8911. 거북이/거북이.java
@@ -1,82 +1,70 @@
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static final int TOP = 0;
-    public static final int RIGHT = 1;
-    public static final int BOTTOM = 2;
-    public static final int LEFT = 3;
-
-
+    public static int front = 0;
+    public static int left = 90;
+    public static int bottom = 180;
+    public static int right = 270;
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
-        String[] lines = new String[N];
 
-        for(int i = 0; i < lines.length; i++) {
-            lines[i] = br.readLine();
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < T; i++) {
+            String walk = br.readLine();
+            bw.write( turtleWalk(walk) + "\n");
         }
-
-        int x;
-        int y;
-        int maxX, maxY;
-        int minX, minY;
-        boolean isX;
-        int status;
-        char ch;
-        String line;
-        for(int k = 0; k < lines.length; k++) {
-            line = lines[k];
-            x = 0;
-            y = 0;
-            maxX = 0;
-            maxY = 0;
-            minX = 0;
-            minY = 0;
-            isX = false;
-            status = TOP;
-            for(int i = 0; i < line.length(); i++) {
-                ch = line.charAt(i);
-
-                if(ch == 'F') {
-                    if(!isX) {
-                        if(status == TOP)
-                            y++;
-                        else y--;
-                    }
-                    else {
-                        if(status == RIGHT)
-                            x++;
-                        else x--;
-                    }
-                }else if(ch == 'L' || ch == 'R') {
-                    isX = !isX;
-                    if(ch == 'L') status -= 1;
-                    if(ch == 'R') status += 1;
-                    if(status > 3) status = 0;
-                    if(status < 0) status = 3;
-                }else if(ch == 'B') {
-                    if(!isX) {
-                        if(status == TOP)
-                            y--;
-                        else y++;
-                    }
-                    else {
-                        if(status == RIGHT)
-                            x--;
-                        else x++;
-                    }
-                }
-                maxX = Math.max(maxX, x);
-                minX = Math.min(minX, x);
-                maxY = Math.max(maxY, y);
-                minY = Math.min(minY, y);
-            }
-            bw.write(((maxX - minX) * (maxY - minY)) + "\n");
-        }
-
         bw.close();
+    }
+
+    public static int turtleWalk(String walk) {
+        int minRow = 0, minCol = 0;
+        int maxRow = 0, maxCol = 0;
+        int row = 0, col = 0;
+        boolean flag = true;
+        int status = front;
+        for(int i = 0; i < walk.length(); i++) {
+            char now = walk.charAt(i);
+            if(now == 'F') {
+                if(status == front) {
+                    col++;
+                }else if(status == bottom) {
+                    col--;
+                }else if(status == left) {
+                    row--;
+                }else if(status == right) {
+                    row++;
+                }
+            }else if(now == 'L') {
+                status += 90;
+                if(status == 360) status = front;
+            }else if(now == 'R') {
+                status -= 90;
+                if(status == -90) status = right;
+            }else if(now == 'B') {
+                if(status == front) {
+                    col--;
+                }else if(status == bottom) {
+                    col++;
+                }else if(status == left) {
+                    row++;
+                }else if(status == right) {
+                    row--;
+                }
+
+            }
+            minRow = Math.min(minRow, row);
+            minCol = Math.min(minCol, col);
+            maxRow = Math.max(maxRow, row);
+            maxCol = Math.max(maxCol, col);
+
+
+        }
+
+
+        return (maxRow - minRow) * (maxCol - minCol);
     }
 
 
