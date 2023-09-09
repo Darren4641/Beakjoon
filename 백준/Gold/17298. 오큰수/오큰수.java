e9295c8ff11851f40f1618f66cd8136f1856ef7d
DaehyunKim <74559561+Darren4641@users.noreply.github.com>
2023-09-09 23:33:30 +0900

    [Gold IV] Title: 오큰수, Time: 1024 ms, Memory: 159036 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -1,65 +1,38 @@
+
 import java.io.*;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static class Node {
-        private int index;
-        private long value;
-
-        public Node(int index, long value) {
-            this.index = index;
-            this.value = value;
-        }
-
-        public int getIndex() { return index; }
-        public long getValue() { return value; }
-    }
-
-    public static long[] solution(long[] num) throws IOException{
-        long[] answer = new long[num.length];
-        Stack<Node> stack = new Stack<Node>();
-
-        stack.push(new Node(0, num[0]));
-        for(int i = 1; i < num.length; i++) {
-            if(stack.peek().getValue() < num[i]) {
-                while(!stack.empty() && stack.peek().getValue() < num[i]) {
-                    answer[stack.pop().getIndex()] = num[i];
-                }
-                stack.push(new Node(i, num[i]));
-            }else {
-                stack.push(new Node(i, num[i]));
-            }
-        }
-        Node node;
-        while(!stack.empty()) {
-            node = stack.pop();
-            answer[node.getIndex()] = -1;
-        }
-        return answer;
-    }
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        try {
-            int size = Integer.parseInt(br.readLine());
-            long[] num = new long[size];
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            for(int i = 0; i < size; i++) {
-                num[i] = Integer.parseInt(st.nextToken());
-            }
-
-            long[] result = solution(num);
-            for(long r : result) {
-                bw.write(r + " ");
-            }
-            bw.close();
-        } catch(IOException e) {
-            e.printStackTrace();
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
         }
 
+        Stack<int[]> s = new Stack<>();
+        int[] answer = new int[N + 1];
+        for(int i = 0; i < N; i++) {
+
+            while(!s.isEmpty() && s.peek()[1] < A[i]) {
+                answer[s.pop()[0] + 1] = A[i];
+            }
+
+            s.push(new int[] {i, A[i]});
+        }
+
+        while(!s.isEmpty()) {
+            int[] p = s.pop();
+            answer[p[0] + 1] = -1;
+        }
+        for(int i = 1; i < answer.length; i++) {
+            bw.write(answer[i] + " ");
+        }
+        bw.close();
     }
 }
