d78dec1fa2c544a71f4d94be0f8c0832282d02ae
DaehyunKim <74559561+Darren4641@users.noreply.github.com>
2023-09-12 23:32:43 +0900

    [Gold IV] Title: 이분 그래프, Time: 1008 ms, Memory: 271072 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.java
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.java
@@ -1,69 +1,61 @@
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int v, e;
-	static ArrayList<Integer>[] al;
-	static int visit[];
+    public static ArrayList<Integer>[] graph;
+    public static int[] visited;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer stz = new StringTokenizer(br.readLine());
-		int t = Integer.parseInt(stz.nextToken());
+        int K = Integer.parseInt(br.readLine());
+        for(int i = 0; i < K; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int V = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
+            graph = new ArrayList[V + 1];
+            visited = new int[V + 1];
+            for(int j = 1; j <= V; j++) {
+                graph[j] = new ArrayList<>();
+            }
+            for(int n = 0; n < E; n++) {
+                st = new StringTokenizer(br.readLine());
+                int s1 = Integer.parseInt(st.nextToken());
+                int s2 = Integer.parseInt(st.nextToken());
+                graph[s1].add(s2);
+                graph[s2].add(s1);
+            }
+            bw.write(bfs(V) ? "YES\n" : "NO\n");
+        }
+        bw.close();
+    }
 
-		for(int tc = 0; tc < t; tc++) {
-			stz = new StringTokenizer(br.readLine());
-			v = Integer.parseInt(stz.nextToken());
-			e = Integer.parseInt(stz.nextToken());
-			visit = new int[v+1];
-			al = new ArrayList[v+1];
+    public static boolean bfs(int V) {
+        Queue<Integer> deque = new LinkedList<>();
+        for(int i = 1; i <= V; i++) {
+            if(visited[i] == 0) {
+                deque.add(i);
+                visited[i] = 1;
+            }
 
-			for(int i = 0; i <= v; i++)
-				al[i] = new ArrayList<Integer>();
+            while(!deque.isEmpty()) {
+                int curNode = deque.poll();
 
-			for(int i = 0; i < e; i++) {
-				stz = new StringTokenizer(br.readLine());
-				int p1 = Integer.parseInt(stz.nextToken());
-				int p2 = Integer.parseInt(stz.nextToken());
+                for(int j = 0; j < graph[curNode].size(); j++) {
+                    int curIndex = graph[curNode].get(j);
+                    if(visited[curIndex] == 0) deque.add(curIndex);
+                    if(visited[curIndex] == visited[curNode]) return false;
 
-				al[p1].add(p2);
-				al[p2].add(p1);
-			}
-			grouping();
-		}
-	}
+                    if(visited[curIndex] == 0 && visited[curNode] == 1)
+                        visited[curIndex] = 2;
+                    else if(visited[curIndex] == 0 && visited[curNode] == 2)
+                        visited[curIndex] = 1;
+                }
+            }
 
-	public static void grouping() {
-		Queue<Integer> q = new LinkedList<Integer>();
-
-		for(int i = 1; i <= v; i++) {
-			if(visit[i] == 0) {
-				q.add(i);
-				visit[i] = 1;
-			}
-
-			while(!q.isEmpty()) {
-				int now = q.poll();
-
-				for(int j = 0; j < al[now].size(); j++) {
-					if(visit[al[now].get(j)] == 0) {
-						q.add(al[now].get(j));
-					}
-					
-					if(visit[al[now].get(j)] == visit[now]) {
-						System.out.println("NO");
-						return;
-					}
-
-					if(visit[now] == 1 && visit[al[now].get(j)] == 0)
-						visit[al[now].get(j)] = 2;
-					else if(visit[now] == 2 && visit[al[now].get(j)] == 0)
-						visit[al[now].get(j)] = 1;
-				}
-			}
-		}
-
-		System.out.println("YES");
-	}
+        }
+        return true;
+    }
 
 }
