da5ab0cb2294c1f55144b0e559c9f066c3c6d6da
DaehyunKim <74559561+Darren4641@users.noreply.github.com>
2023-09-12 12:16:44 +0900

    [Gold III] Title: 크게 만들기, Time: 400 ms, Memory: 42484 KB -BaekjoonHub

---
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        Deque<Integer> deque = new LinkedList<>();
+        String num = br.readLine();
+
+        for(int i = 0; i < N; i++) {
+            //1231234
+            int curNum = num.charAt(i) - '0';
+            while(!deque.isEmpty() && K != 0 && deque.getLast() < curNum) {
+                deque.removeLast();
+                K--;
+            }
+            deque.addLast(curNum);
+        }
+
+        while(deque.size() > K) {
+            bw.write(deque.removeFirst() + "");
+        }
+        bw.close();
+    }
+
+}
