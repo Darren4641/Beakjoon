ba5adf3fecc86017b0a3b9abb76a0ceaff9874f1
김대현 <74559561+Darren4641@users.noreply.github.com>
2023-10-19 21:03:42 +0900

    [Silver V] Title: 방 번호, Time: 128 ms, Memory: 14376 KB -BaekjoonHub

---
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int[] num = new int[10];
+        String N = br.readLine();
+
+        for(int i = 0; i < N.length(); i++) {
+            int current = N.charAt(i) - '0';
+            if(current == 6 && num[6] > num[9]) num[9]++;
+            else if(current == 9 && num[9] > num[6]) num[6]++;
+            else num[current]++;
+        }
+        int answer = 0;
+        for(int i = 0; i < num.length; i++) {
+            answer = Math.max(answer, num[i]);
+        }
+        bw.write(answer + "");
+        bw.close();
+    }
+}
