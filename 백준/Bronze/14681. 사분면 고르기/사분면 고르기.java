aeeb0fd85278c67b36b0e709f86355a8bba743a0
김대현 <74559561+Darren4641@users.noreply.github.com>
2023-11-26 21:50:16 +0900

    [Bronze V] Title: 사분면 고르기, Time: 128 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int X = Integer.parseInt(br.readLine());
+        int Y = Integer.parseInt(br.readLine());
+
+        if(X > 0 && Y > 0) bw.write("1");
+        else if(X > 0 && Y < 0) bw.write("4");
+        else if(X < 0 && Y > 0) bw.write("2");
+        else if(X < 0 && Y < 0) bw.write("3");
+
+        bw.close();
+    }
+}
