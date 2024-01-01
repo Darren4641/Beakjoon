e203a27b2fad9278f53bf7905fb801330abbdd9d
김대현 <74559561+Darren4641@users.noreply.github.com>
2024-01-01 17:39:30 +0900

    [Bronze II] Title: 수 정렬하기, Time: 160 ms, Memory: 16216 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,38 +1,22 @@
 import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void solution(int[] num) throws IOException{
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int temp;
-        for(int i = 0; i < num.length - 1; i++) {
-            for(int j = 0; j <num.length - 1 - i; j++) {
-                if(num[j] > num[j + 1]) {
-                    temp = num[j];
-                    num[j] = num[j + 1];
-                    num[j + 1] = temp;
-                }
-            }
-        }
-        for(int i = 0; i < num.length; i++) {
-            bw.write(num[i] + "\n");
-        }
-        bw.close();
-    }
-
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        try {
-            int size = Integer.parseInt(br.readLine());
-            int[] num = new int[size];
-            for(int i = 0 ; i < size; i++) {
-                num[i] = Integer.parseInt(br.readLine());
-            }
-            solution(num);
-        } catch(IOException e) {
-            e.printStackTrace();
-        }
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int index = 0;
+        while(N-- > 0) {
+            int num = Integer.parseInt(br.readLine());
+            arr[index++] = num;
+        }
+        Arrays.sort(arr);
+        for(int n : arr) bw.write(n + "\n");
+        bw.close();
     }
 }
